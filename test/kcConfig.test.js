89361b6a15707e2a6775006fb54374555d5cb0b1
onigra <3280467rec@gmail.com>
2018-03-30 15:28:58 +0900

    add redirectUri test

--- test/kcConfig.test.js
+++ test/kcConfig.test.js
@@ -2,17 +2,19 @@ import KcConfig from '~/src/kcConfig'
 import assert from 'assert'
 
 describe('KcConfig', () => {
-  test('constructor', () => {
-    const config = {
-      realm: "my-realm",
-      url: "http://localhost:8080/auth",
-      clientId: "my-realm-client"
-    }
+  const config = {
+    realm: "my-realm",
+    url: "https://localhost:8080/auth",
+    clientId: "my-realm-client",
+    redirectUri: "https://localhost:3000/"
+  }
 
+  test('constructor', () => {
     const kcConfig = new KcConfig(config)
 
     assert(kcConfig.realm === "my-realm")
-    assert(kcConfig.url === "http://localhost:8080/auth")
+    assert(kcConfig.url === "https://localhost:8080/auth")
     assert(kcConfig.clientId === "my-realm-client")
+    assert(kcConfig.redirectUri === "https://localhost:3000/")
   })
 })
