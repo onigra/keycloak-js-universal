bc5ff1791d9e54961e86ee7764a25533730251ed
onigra <3280467rec@gmail.com>
2018-03-30 15:52:06 +0900

    add url generator methods

--- src/kcConfig.js
+++ src/kcConfig.js
@@ -12,6 +12,22 @@ class KcConfig {
     this.clientId = props.clientId
     this.redirectUri = props.redirectUri
   }
+
+  authUrl() {
+    return this.url
+      + '/realms/' + this.realm
+      + '/protocol/openid-connect/auth'
+      + '?client_id=' + encodeURIComponent(this.clientId)
+      + '&redirect_uri=' + encodeURIComponent(this.redirectUri)
+  }
+
+  registrationUrl() {
+    return this.url
+      + '/realms/' + this.realm
+      + '/protocol/openid-connect/registrations'
+      + '?client_id=' + encodeURIComponent(this.clientId)
+      + '&redirect_uri=' + encodeURIComponent(this.redirectUri)
+  }
 }
 
 module.exports = KcConfig
