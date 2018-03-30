b1828d15cfd615b2cb2e72dc180159e779a54ab5
onigra <3280467rec@gmail.com>
2018-03-30 15:21:10 +0900

    add redirectUri

--- src/kcConfig.js
+++ src/kcConfig.js
@@ -4,11 +4,13 @@ class KcConfig {
   realm: string;
   url: string;
   clientId: string;
+  redirectUri: string
 
-  constructor(props: { realm: string, url: string, clientId: string }) {
+  constructor(props: { realm: string, url: string, clientId: string, redirectUri: string }) {
     this.realm = props.realm
     this.url = props.url
     this.clientId = props.clientId
+    this.redirectUri = props.redirectUri
   }
 }
 
