97e89da87ecfe1b79d9fe55d07bcbef2c8a2360f
onigra <3280467rec@gmail.com>
2018-03-31 11:51:57 +0900

    add flow 3 context tests

--- test/keycloakInitOptions.test.js
+++ test/keycloakInitOptions.test.js
@@ -2,7 +2,7 @@ import KeycloakInitOptions from '~/src/keycloakInitOptions'
 import assert from 'assert'
 
 describe('KeycloakInitOptions', () => {
-  test('constructor', () => {
+  test('flow is standard', () => {
     const options = {
       responseMode: 'fragment',
       flow: 'standard',
@@ -12,5 +12,27 @@ describe('KeycloakInitOptions', () => {
     assert(initOptions.responseMode === 'fragment')
     assert(initOptions.flow === 'standard')
     assert(initOptions.responseType === 'code')
+  })
+
+  test('flow is standard', () => {
+    const options = {
+      responseMode: 'fragment',
+      flow: 'implicit',
+    }
+    const initOptions = new KeycloakInitOptions(options)
+
+    assert(initOptions.flow === 'implicit')
+    assert(initOptions.responseType === 'id_token token')
+  })
+
+  test('flow is hybrid', () => {
+    const options = {
+      responseMode: 'fragment',
+      flow: 'hybrid',
+    }
+    const initOptions = new KeycloakInitOptions(options)
+
+    assert(initOptions.flow === 'hybrid')
+    assert(initOptions.responseType === 'code id_token token')
   })
 })
