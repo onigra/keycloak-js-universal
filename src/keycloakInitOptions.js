fedea744d7896fd73b942761045bd794df01e64a
onigra <3280467rec@gmail.com>
2018-04-01 00:14:19 +0900

    add onLoad

--- src/keycloakInitOptions.js
+++ src/keycloakInitOptions.js
@@ -3,11 +3,13 @@
 type ResponseMode = 'query' | 'fragment'
 type Flow = 'standard' | 'implicit' | 'hybrid'
 type ResponseType = 'code' | 'id_token token' | 'code id_token token'
+type OnLoad = 'login-required' | 'check-sso'
 
 class KeycloakInitOptions {
   responseMode: ResponseMode
   flow: Flow
   responseType: ResponseType
+  onLoad: OnLoad
 
   static RESPONSE_TYPE = {
     standard: 'code',
@@ -15,10 +17,11 @@ class KeycloakInitOptions {
     hybrid: 'code id_token token',
   }
 
-  constructor(options: { responseMode: ResponseMode, flow: Flow }) {
+  constructor(options: { responseMode: ResponseMode, flow: Flow, onLoad: OnLoad }) {
     this.responseMode = options.responseMode
     this.flow = options.flow
     this.responseType = KeycloakInitOptions.RESPONSE_TYPE[options.flow]
+    this.onLoad = options.onLoad
   }
 }
 
--- test/keycloakInitOptions.test.js
+++ test/keycloakInitOptions.test.js
@@ -6,33 +6,39 @@ describe('KeycloakInitOptions', () => {
     const options = {
       responseMode: 'fragment',
       flow: 'standard',
+      onLoad: 'login-required',
     }
     const initOptions = new KeycloakInitOptions(options)
 
     assert(initOptions.responseMode === 'fragment')
     assert(initOptions.flow === 'standard')
     assert(initOptions.responseType === 'code')
+    assert(initOptions.onLoad === 'login-required')
   })
 
   test('flow is standard', () => {
     const options = {
       responseMode: 'fragment',
       flow: 'implicit',
+      onLoad: 'check-sso',
     }
     const initOptions = new KeycloakInitOptions(options)
 
     assert(initOptions.flow === 'implicit')
     assert(initOptions.responseType === 'id_token token')
+    assert(initOptions.onLoad === 'check-sso')
   })
 
   test('flow is hybrid', () => {
     const options = {
       responseMode: 'fragment',
       flow: 'hybrid',
+      onLoad: 'check-sso',
     }
     const initOptions = new KeycloakInitOptions(options)
 
     assert(initOptions.flow === 'hybrid')
     assert(initOptions.responseType === 'code id_token token')
+    assert(initOptions.onLoad === 'check-sso')
   })
 })
