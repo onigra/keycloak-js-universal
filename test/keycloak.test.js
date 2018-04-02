1baf21bac10a1a57e88d2384fec1e7c255d7d513
onigra <3280467rec@gmail.com>
2018-04-02 16:16:26 +0900

    生成されるURLにUUIDが入ってくるのでtoMatchのテストにした

--- test/keycloak.test.js
+++ test/keycloak.test.js
@@ -10,22 +10,30 @@ describe('Keycloak', () => {
   }
 
   test('createLoginUrl', () => {
-    const expected =
-      'https://localhost:8080' +
-      '/auth/realms/my-realm/protocol/openid-connect/auth' +
-      '?client_id=my-realm-client&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F'
+    const urlRegexp = 'https:\/\/localhost:8080' +
+      '\/auth\/realms\/my-realm\/protocol\/openid-connect\/auth' +
+      '\\?client_id=my-realm-client&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
+      '&state=([0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})' +
+      '&response_mode=fragment' +
+      '&response_type=code' +
+      '&scope=openid'
+    const expected = new RegExp(urlRegexp)
 
     const keycloak = new Keycloak(config)
-    assert(expected === keycloak.createLoginUrl())
+    expect(keycloak.createLoginUrl()).toMatch(expected)
   })
 
   test('createRegisterUrl', () => {
-    const expected =
-      'https://localhost:8080' +
-      '/auth/realms/my-realm/protocol/openid-connect/registrations' +
-      '?client_id=my-realm-client&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F'
+    const urlRegexp = 'https:\/\/localhost:8080' +
+      '\/auth\/realms\/my-realm\/protocol\/openid-connect\/registrations' +
+      '\\?client_id=my-realm-client&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
+      '&state=([0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})' +
+      '&response_mode=fragment' +
+      '&response_type=code' +
+      '&scope=openid'
+    const expected = new RegExp(urlRegexp)
 
     const keycloak = new Keycloak(config)
-    assert(expected === keycloak.createRegisterUrl())
+    expect(keycloak.createRegisterUrl()).toMatch(expected)
   })
 })
