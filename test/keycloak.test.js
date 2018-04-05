fd8d578b59dc8e66ed3a60e3e06e76d6d0b03e7c
onigra <3280467rec@gmail.com>
2018-04-06 08:53:29 +0900

    modify scope parameter position

--- test/keycloak.test.js
+++ test/keycloak.test.js
@@ -10,13 +10,15 @@ describe('Keycloak', () => {
   }
 
   test('createLoginUrl', () => {
-    const urlRegexp = 'https:\/\/localhost:8080' +
-      '\/auth\/realms\/my-realm\/protocol\/openid-connect\/auth' +
-      '\\?client_id=my-realm-client&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
+    const urlRegexp =
+      'https://localhost:8080' +
+      '/auth/realms/my-realm/protocol/openid-connect/auth' +
+      '\\?client_id=my-realm-client' +
+      '&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
+      '&scope=openid' +
       '&state=([0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})' +
       '&response_mode=fragment' +
-      '&response_type=code' +
-      '&scope=openid'
+      '&response_type=code'
     const expected = new RegExp(urlRegexp)
 
     const keycloak = new Keycloak(config)
@@ -24,13 +26,15 @@ describe('Keycloak', () => {
   })
 
   test('createRegisterUrl', () => {
-    const urlRegexp = 'https:\/\/localhost:8080' +
-      '\/auth\/realms\/my-realm\/protocol\/openid-connect\/registrations' +
-      '\\?client_id=my-realm-client&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
+    const urlRegexp =
+      'https://localhost:8080' +
+      '/auth/realms/my-realm/protocol/openid-connect/registrations' +
+      '\\?client_id=my-realm-client' +
+      '&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
+      '&scope=openid' +
       '&state=([0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})' +
       '&response_mode=fragment' +
-      '&response_type=code' +
-      '&scope=openid'
+      '&response_type=code'
     const expected = new RegExp(urlRegexp)
 
     const keycloak = new Keycloak(config)
