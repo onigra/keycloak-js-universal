5cb1d8e10ea9d5765b2040233289637898dbcc75
onigra <3280467rec@gmail.com>
2018-04-08 17:32:34 +0900

    とりあえずコンストラクタにリダイレクトURIを全て指定するようにした

--- src/keycloak.js
+++ src/keycloak.js
@@ -12,23 +12,13 @@ export default class Keycloak {
     realm: string,
     url: string,
     clientId: string,
-    redirectUri: string,
+    loginRedirectUri: string,
+    logoutRedirectUri: string,
+    signupRedirectUri: string,
     scope: string | void,
   }) {
     this.config = new KeycloakConfig(config)
     this.options = new KeycloakOptions()
-  }
-
-  createLoginUrl() {
-    return (
-      this.config.authUrl() +
-      '&state=' +
-      encodeURIComponent(createUUID()) +
-      '&response_mode=' +
-      encodeURIComponent(this.options.responseMode) +
-      '&response_type=' +
-      encodeURIComponent(this.options.responseType)
-    )
   }
 
   createRegisterUrl() {
--- test/keycloak.test.js
+++ test/keycloak.test.js
@@ -6,31 +6,17 @@ describe('Keycloak', () => {
     realm: 'my-realm',
     url: 'https://localhost:8080/auth',
     clientId: 'my-realm-client',
-    redirectUri: 'https://localhost:3000/',
+    loginRedirectUri: 'https://localhost:3000/callback/login',
+    logoutRedirectUri: 'https://localhost:3000/callback/logout',
+    signupRedirectUri: 'https://localhost:3000/callback/signup',
   }
-
-  test('createLoginUrl', () => {
-    const urlRegexp =
-      'https://localhost:8080' +
-      '/auth/realms/my-realm/protocol/openid-connect/auth' +
-      '\\?client_id=my-realm-client' +
-      '&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
-      '&scope=openid' +
-      '&state=([0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})' +
-      '&response_mode=fragment' +
-      '&response_type=code'
-    const expected = new RegExp(urlRegexp)
-
-    const keycloak = new Keycloak(config)
-    expect(keycloak.createLoginUrl()).toMatch(expected)
-  })
 
   test('createRegisterUrl', () => {
     const urlRegexp =
       'https://localhost:8080' +
       '/auth/realms/my-realm/protocol/openid-connect/registrations' +
       '\\?client_id=my-realm-client' +
-      '&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F' +
+      '&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fcallback%2Fsignup' +
       '&scope=openid' +
       '&state=([0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})' +
       '&response_mode=fragment' +
